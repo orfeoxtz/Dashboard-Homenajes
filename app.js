077cab00105e1b9280cdd7fbf7f6fe35690312e6
orfeoxtz <jorgekorfan@gmail.com>
2026-06-07 16:49:41 -0500

    Update app.js

--- app.js
+++ app.js
@@ -4,7 +4,7 @@
 <meta charset="UTF-8">
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
-<title>General Report Jkfh | 4K Premium</title>
+<title>General Report Jorg korf4n</title>
 
 <link rel="stylesheet" href="style.css">
 <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.2/css/all.min.css" rel="stylesheet">
